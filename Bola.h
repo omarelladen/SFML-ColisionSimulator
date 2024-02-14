1f716ebd694b413cbe92c485e7b683e464ee4f8d
omarelladen <109101498+omarelladen@users.noreply.github.com>
2024-02-14 19:12:20 -0300

    Add files via upload
    0

---
+++ Bola.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+using namespace sf;
+
+class Bola
+{
+public:
+	CircleShape corpo;
+
+	float raio;
+	Vector2f pos;
+	Vector2f vel;
+
+
+	Bola();
+	~Bola();
+
+	void setRaio(float raio);
+	void setVel(Vector2f v);
+
+	void mover();
+	void executar();
+};
+
