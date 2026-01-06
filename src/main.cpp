afe663dfeb3e339be556283445407ccf349173c4
Omar Zagonel El Laden <omarzladen@gmail.com>
2026-01-06 19:20:11 -0300

    add individual param check

--- src/main.cpp
+++ src/main.cpp
@@ -30,18 +30,43 @@ int main()
 
     int num_balls = 0;
     readInput(&num_balls, 3, "Number of balls");
+    if (num_balls < 1)
+    {
+        std::cout << "Minimum number is 1!" << std::endl;
+        return 0;
+    }
 
     int r;
     readInput(&r, 30, "Ball radius");
+    if (r < 1)
+    {
+        std::cout << "Minimum radius is 1!" << std::endl;
+        return 0;
+    }
 
     int m;
     readInput(&m, 1, "Ball mass");
+    if (m < 1)
+    {
+        std::cout << "Minimum mass is 1!" << std::endl;
+        return 0;
+    }
 
     int win_w;
     readInput(&win_w, 500, "Window width");
+    if (win_w < r)
+    {
+        std::cout << "Minimum size is r!" << std::endl;
+        return 0;
+    }
 
     int win_h;
     readInput(&win_h, 500, "Window height");
+    if (win_h < r)
+    {
+        std::cout << "Minimum size is r!" << std::endl;
+        return 0;
+    }
 
 
     if (2*r >= win_h || 2*r >= win_w)
